cedd8638d327f534a13108262b69ae15bb93c643
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-10 17:41:03 -0600

    just a commit ;)

--- 9.1_Min_Function.py
+++ 9.1_Min_Function.py
@@ -26,3 +26,28 @@ The function should return the value, not print the value.
 Also, while there is a min function built into Python, don't use it. 
 Please use if statements and practice creating it yourself.
 '''
+
+l1 = (7, 3, 5)
+l2 = (5, 5, 4)
+l3 = (2, 2, 3)
+l4 = (-2, -6, -100)
+l5 = ("Z", "B", "A")
+
+def min_val(an):
+    if an[0] < an[1]:
+        print(an[0])
+    elif an[1] < an[2]:
+        print(an[1])
+    elif an[2] < an[0]:
+        print(an[2])
+    return print
+
+
+an = input("I will print the min for you what list would you like? l1, l2, l3, l4 or l5: ")
+
+if an.lower() == "l1":
+    print("helo")
+# n = min_val(an)
+# print("this is the min: ", n)
+
+print(an)
