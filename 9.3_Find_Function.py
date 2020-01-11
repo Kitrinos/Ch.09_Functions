b5588cb2411bcb927acd2487b2d697cdbdaee0a5
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-11 14:53:31 -0600

    just a commit ;)

--- 9.3_Find_Function.py
+++ 9.3_Find_Function.py
@@ -31,3 +31,24 @@ Use a for loop with an index variable and a range.
 Inside the loop use an if statement. This function 
 can be written in about four lines of code.
 '''
+
+list = [36, 31, 79, 96, 36, 91, 77, 33, 19, 3, 34, 12, 70, 12, 54, 98, 86, 11, 17, 17]
+
+def find_num(list):
+    for i in list:
+        print("What numbers would you like me to find?")
+        numbers = int(input("Please input your numbers : "))
+        if int == numbers:
+            print("Found",  numbers, "at position")
+        return numbers
+        break
+
+n = find_num(list)
+
+
+
+
+
+
+
+
