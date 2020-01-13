98fb0a7931212c3183db97be12ea7ac1d819aae1
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-12 19:20:19 -0600

    just a commit ;)

--- 9.5_Fibonacci.py
+++ 9.5_Fibonacci.py
@@ -11,20 +11,20 @@ Just to do a quick review of text formatting in the last chapter, make the list 
 right-justified with commas.
 '''
 
-def Fibonacci(n):
-    a = 0
-    b = 1
-    if n == 1:
-        print(b)
-    else:
-        print(a)
-        print(b)
-        for i in range(1, 50):
-            c = a + b
-            a = b
-            b = c
-            print(c)
-
-Fibonacci(0)
+# def Fibonacci(n):
+#     a = 0
+#     b = 1
+#     if n == 1:
+#         print(b)
+#     else:
+#         print(a)
+#         print(b)
+#         for i in range(1, 50):
+#             c = a + b
+#             a = b
+#             b = c
+#             print(c)
+#
+# Fibonacci(0)
 
 
