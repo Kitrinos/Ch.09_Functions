7d9c03842abf08c14c0ab2dec511dc14d06c90c0
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-12 19:21:14 -0600

    just a commit ;)

--- 9.5_Fibonacci.py
+++ 9.5_Fibonacci.py
@@ -14,16 +14,13 @@ right-justified with commas.
 # def Fibonacci(n):
 #     a = 0
 #     b = 1
-#     if n == 1:
-#         print(b)
-#     else:
-#         print(a)
-#         print(b)
-#         for i in range(1, 50):
-#             c = a + b
-#             a = b
-#             b = c
-#             print(c)
+#     print(a)
+#     print(b)
+#     for i in range(1, 50):
+#         c = a + b
+#         a = b
+#         b = c
+#         print(c)
 #
 # Fibonacci(0)
 
