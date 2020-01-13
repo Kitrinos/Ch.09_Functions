5489db91ca9a9cf0a734befd23b81d2715bba47f
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-13 09:54:18 -0600

    just a commit ;)

---
+++ prac.py
@@ -0,0 +1,12 @@
+#Hang man
+
+l = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"]
+
+an = input("What would you like your word to be: ")
+al = len(an)
+
+print()
+print(al * "_ ")
+
+g = input("what would you like to guess?: ")
+
