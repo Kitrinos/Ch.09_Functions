e9bb6b03e570bea9ceafff9215d26dceb6e821d8
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-15 16:36:46 -0600

    just a commit ;)

--- prac.py
+++ prac.py
@@ -1,15 +1,16 @@
-#Hang man
+# Hang man
+import re
+word = input("Enter a phrase: ")
+dashes = "-" * len(word)
+lol = list(dashes)
+done = False
 
-# word = input("Enter a phrase: ")
-# dashes = "-" * len(word)
-# lol = list(dashes)
-# done = False
-#
-# while not done:
-#     print()
-#     print("guess this word/phrase?:", lol)
-#     letter = str(input("Enter a letter or the answer(s) :"))
-#     if letter in word:
-#         get_position = word.index(letter)
-#         # print(get_position)
-#         lol[get_position] = letter
+while not done:
+    print()
+    print("guess this word/phrase?: ")
+    print(' '.join(lol))
+    letter = str(input("Enter a letter or the answer(s) :"))
+    if letter in word:
+        get_position = word.index(letter)
+        # print(get_position)
+        lol[get_position] = letter
