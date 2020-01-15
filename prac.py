510a7bec663757e777e196f403b4a5244829db7c
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-15 16:50:33 -0600

    just a commit ;)

--- prac.py
+++ prac.py
@@ -1,16 +1,18 @@
-# Hang man
-import re
+# Hang man, add spaces, and pics
+
 word = input("Enter a phrase: ")
 dashes = "-" * len(word)
 lol = list(dashes)
 done = False
 hangman = 0
+wronglist = [""]
 
 while not done:
     print()
     print("guess this word/phrase?: ")
     print(' '.join(lol))
     print()
+    print("Your guesses: ", ','.join(wronglist))
     letter = str(input("Enter a letter or the answer(s) : "))
     if letter in word:
         get_position = word.index(letter)
@@ -18,6 +20,7 @@ while not done:
     elif letter != word:
         print(letter, "is not in the word, try again!")
         hangman+=1
+        wronglist.append(letter)
     else:
         print("sorry that is not an option")
 
