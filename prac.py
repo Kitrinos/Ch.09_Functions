80408b880acfeeb75aeb57fd33f945c1f53ff708
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-15 16:38:18 -0600

    just a commit ;)

--- prac.py
+++ prac.py
@@ -4,6 +4,7 @@ word = input("Enter a phrase: ")
 dashes = "-" * len(word)
 lol = list(dashes)
 done = False
+hangman = 0
 
 while not done:
     print()
@@ -12,5 +13,9 @@ while not done:
     letter = str(input("Enter a letter or the answer(s) :"))
     if letter in word:
         get_position = word.index(letter)
-        # print(get_position)
         lol[get_position] = letter
+    elif letter != word:
+        print("That letter is not in the word, try again!")
+        hangman+=1
+    else:
+        print("sorry that is not an option")
