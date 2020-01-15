387f31057e0610b1918bbc6d344211a7293a4046
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-15 16:40:50 -0600

    just a commit ;)

--- prac.py
+++ prac.py
@@ -10,12 +10,15 @@ while not done:
     print()
     print("guess this word/phrase?: ")
     print(' '.join(lol))
-    letter = str(input("Enter a letter or the answer(s) :"))
+    print()
+    letter = str(input("Enter a letter or the answer(s) : "))
     if letter in word:
         get_position = word.index(letter)
         lol[get_position] = letter
     elif letter != word:
         print("That letter is not in the word, try again!")
         hangman+=1
+        print(letter)
     else:
         print("sorry that is not an option")
+
