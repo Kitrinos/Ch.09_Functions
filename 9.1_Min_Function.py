79e8eea1026fcde44c76a3d1c8ed09bf9e70867e
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-16 14:09:15 -0600

    just a commit ;)

--- 9.1_Min_Function.py
+++ 9.1_Min_Function.py
@@ -26,18 +26,18 @@ The function should return the value, not print the value.
 Also, while there is a min function built into Python, don't use it. 
 Please use if statements and practice creating it yourself.
 '''
-l6 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", 'Z']
-
-def min_val(num0, num1, num2):
-    if num0 < num1:
-        return num0
-    elif num1 < num2:
-        return num1
-    elif num2 < num0:
-        return num2
-
-print(min_val(7, 3, 5))
-print(min_val(5, 5, 4))
-print(min_val(2, 2, 3))
-print(min_val(-2, -6, -100))
-print(min_val("Z", "B", "A"))
+# l6 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", 'Z']
+#
+# def min_val(num0, num1, num2):
+#     if num0 < num1:
+#         return num0
+#     elif num1 < num2:
+#         return num1
+#     elif num2 < num0:
+#         return num2
+#
+# print(min_val(7, 3, 5))
+# print(min_val(5, 5, 4))
+# print(min_val(2, 2, 3))
+# print(min_val(-2, -6, -100))
+# print(min_val("Z", "B", "A"))
