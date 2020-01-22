c65984bb23aa2df1af10742df5a3c6ae72459369
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-22 14:58:53 -0600

    just a commit ;)

--- 9.7_BB8.py
+++ 9.7_BB8.py
@@ -24,12 +24,12 @@ def draw_BB8(x,y, radius): # Function to draw BB8 robots
     arcade.draw_circle_outline(x, y, radius/1.5, arcade.color.BLACK, 2)
     arcade.draw_circle_outline(x, y, radius/4, arcade.color.BLACK, 2)
     #HEAD
-    arcade.draw_arc_filled(x, y + radius/1.4, radius - radius/3, radius - radius/3, arcade.color.WHITE, 40, 220, -40)
-    arcade.draw_arc_outline(x, y + radius/1.4, radius - radius/3 , radius - radius/3, arcade.color.BLACK, 0, 180, 2)
-    arcade.draw_line(x - radius/1.5, y + radius/1.4, x + radius/1.5, y + radius/1.4, arcade.color.BLACK, 2)
+    arcade.draw_arc_filled(x, y + radius/1.2, radius - radius/3, radius - radius/3, arcade.color.WHITE, 40, 220, -40)
+    arcade.draw_arc_outline(x, y + radius/1.2, radius - radius/3 , radius - radius/3, arcade.color.BLACK, 0, 180, 2)
+    arcade.draw_line(x - radius/1.5, y + radius/1.2, x + radius/1.5, y + radius/1.2, arcade.color.BLACK, 2)
     #HEADCIRCLE
-    arcade.draw_circle_filled(x, y + radius, radius - radius/1.2, arcade.color.LIGHT_BLUE)
-    arcade.draw_circle_outline(x, y + radius, radius - radius/1.2, arcade.color.BLACK, 2)
+    arcade.draw_circle_filled(x, y + radius + radius/5, radius - radius/1.2, arcade.color.AIR_SUPERIORITY_BLUE)
+    arcade.draw_circle_outline(x, y + radius + radius/5, radius - radius/1.2, arcade.color.BLACK, 2)
 #The main function where we set background color, start and finish rendering and run.
 def main():
     arcade.set_background_color(arcade.color.WHEAT)
