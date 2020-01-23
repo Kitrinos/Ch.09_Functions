0c332aba8039bbba5613d71338dcab3f2daf1c5c
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-23 14:11:45 -0600

    just a commit ;)

--- 9.4_Fizzbuzz.py
+++ 9.4_Fizzbuzz.py
@@ -33,23 +33,21 @@ fizzbuzz
 
 The classic test is to use the numbers 1-100 so make sure you test that with your function.
 '''
-# n = 15
-#
-# def fizzbuzz(n):
-#     '''prints fizz for muliples of three, prints buzz for multiples of five, and prints fizzbuzz for multiples of five and three'''
-#     for i in range(1, 16):
-#         if i % 3 == 0 and i % 5 == 0:
-#             print("fizzbuzz")
-#             continue
-#         elif i % 3 == 0:
-#             print("fizz")
-#             continue
-#         elif i % 5 == 0:
-#             print("buzz")
-#             continue
-#         print(i)
-#     return fizzbuzz
-#
-#
-# fizzbuzz(n)
+def fizzbuzz(n):
+    '''prints fizz for muliples of three, prints buzz for multiples of five, and prints fizzbuzz for multiples of five and three'''
+    for i in range(n):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+            continue
+        elif i % 3 == 0:
+            print("fizz")
+            continue
+        elif i % 5 == 0:
+            print("buzz")
+            continue
+        print(i)
+    return fizzbuzz
+
+
+fizzbuzz(101)
 
