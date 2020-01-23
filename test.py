14beb5b3ec325eb9b6052ec67ac807c322b72730
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-01-23 14:00:16 -0600

    just a commit ;)

--- test.py
+++ test.py
@@ -1,12 +1,33 @@
 import math
 
-def volume_sphere(r):
-    volume = 4/3*math.pi*r**3
-    return volume
+# def volume_sphere(r):
+#     volume = 4/3*math.pi*r**3
+#     return volume
+#
+# def main():
+#     volume = volume_sphere(2)
+#     print(f"Your volume is {volume:.2f} m^3")
+#
+# if __name__ == "__main__":
+#     main()
 
-def main():
-    volume = volume_sphere(2)
-    print(f"Your volume is {volume:.2f} m^3")
 
-if __name__ == "__main__":
-    main()
+# def f(leg1, leg2,):
+#     '''caculate the hypotenuse of a triangle'''
+#     hypo = math.sqrt(leg1**2+leg2**2)
+#     print(hypo)
+#
+# f(2,3)
+
+# def meanfinder(a, b, c):
+#     ave = (a+b+c)/3.0
+#     print(ave)
+#
+# meanfinder(1,2,3)
+
+# def prim(b,h):
+#     per = (2*b) + (2*h)
+#     print(per)
+#
+# prim(1,2)
+
